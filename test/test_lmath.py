a8289f751cd56477376e3369faabeefc03bbfd76
user <stiles.trenton@gmail.com>
2023-06-27 20:14:22 -0500

    implement tests

--- test/test_lmath.py
+++ test/test_lmath.py
@@ -1,5 +1,16 @@
-import lmath.lmath
+import sys
+sys.path.append('..')  # hack to import module in parent dir...
+from lmath import lmath
 import pytest
-def test_add():
 
-    pass
+def test_add():
+    assert lmath.add(5,5) == 10
+
+def test_sub():
+    assert lmath.sub(10,5) == 5
+
+def test_mult():
+    assert lmath.mult(5,5) == 25
+
+def test_div():
+    assert lmath.div(5,5) == 1
